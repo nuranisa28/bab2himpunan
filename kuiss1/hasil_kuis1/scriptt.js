f965a435b5ca015a7bc42830cadd7eb35460aa6b
nuranisa28 <nisa.aja66@gmail.com>
2020-08-24 11:04:20 +0800

    bab 2

---
+++ kuiss1/hasil_kuis1/scriptt.js
@@ -0,0 +1,100 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyBDwro-fWKgKk_8qR1_AnWQtWuYWyf_NMc",
+    authDomain: "kuis1-32094.firebaseapp.com",
+    databaseURL: "https://kuis1-32094.firebaseio.com",
+    projectId: "kuis1-32094",
+    storageBucket: "kuis1-32094.appspot.com",
+    messagingSenderId: "968246848051",
+    appId: "1:968246848051:web:c6c755c17edc51187fd146"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+window.load = function () {
+
+
+}
+
+function readlah() {
+    var task = firebase.database().ref("kuis1/");
+    let tmp = document.querySelector('.disini');
+    tmp.innerHTML = "";
+    kelasnya = document.getElementById('kelas');
+    sekolah = document.getElementById('sekolah');
+    let kelasfix = '';
+    let sekolahfix = '';
+
+    if (kelasnya.value == "1") {
+        kelasfix = "7A";
+    } else if (kelasnya.value == "2") {
+        kelasfix = "7B";
+    } else if (kelasnya.value == "3") {
+        kelasfix = "7C";
+    } else if (kelasnya.value == "4") {
+        kelasfix = "7D";
+    } else if (kelasnya.value == "5") {
+        kelasfix = "7E";
+    } else if (kelasnya.value == "6") {
+        kelasfix = "7F";
+    } else if (kelasnya.value == "7") {
+        kelasfix = "7G";
+    } else if (kelasnya.value == "8") {
+        kelasfix = "7H";
+    } else if (kelasnya.value == "9") {
+        kelasfix = "7I";
+    }
+
+    if (sekolah.value == "1") {
+        sekolahfix = "SMP Negeri 2 Banjarmasin";
+    } else if (sekolah.value == "2") {
+        sekolahfix = "SMP Negeri 9 Banjarmasin";
+    }
+
+    if ((sekolahfix != '') && (kelasfix != '')) {
+        task.on("child_added", function (data) {
+            var taskvalue = data.val();
+
+
+            if ((sekolahfix == taskvalue.sekolah) && (kelasfix == taskvalue.kelas)) {
+                tmp.innerHTML += `<tr>
+                            <td class="ukr1">${taskvalue.sekolah}</td>
+                            <td class="ukr3">${taskvalue.kelas}</td>
+                            <td class="ukr1">${taskvalue.nama}</td>
+                            <td class="ukr3">${taskvalue.nilai}</td>
+                            <td class="ukr2">${taskvalue.hari}</td>
+                            <td class="ukr2">${taskvalue.waktu}</td>
+                        </tr>`;
+            }
+        });
+
+    } else if (sekolahfix != '') {
+        task.on("child_added", function (data) {
+            var taskvalue = data.val();
+
+
+            if ((sekolahfix == taskvalue.sekolah)) {
+                tmp.innerHTML += `<tr>
+                            <td class="ukr1">${taskvalue.sekolah}</td>
+                            <td class="ukr3">${taskvalue.kelas}</td>
+                            <td class="ukr1">${taskvalue.nama}</td>
+                            <td class="ukr3">${taskvalue.nilai}</td>
+                            <td class="ukr2">${taskvalue.hari}</td>
+                            <td class="ukr2">${taskvalue.waktu}</td>
+                        </tr>`;
+            }
+        });
+
+    } else {
+        alert('Tentukan filter pencarian');
+    }
+
+
+
+}
+
+window.onload = function () {
+    kelasnya = document.getElementById('kelas');
+    sekolah = document.getElementById('sekolah');
+    kelasnya.value = value = "0";
+    sekolah.value = value = "0";
+}
